f8855e7862b6e2bba558cf68c21974f1e7fba3cb
Chris Wood <wchristopherwood912@gmail.com>
2023-02-21 10:28:22 -0800

    Updates

---
+++ precache-manifest.0737d204fdc340e2b8351c4af7baf5f3.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "c6edae855e838d003954",
+    "url": "/sfpopos/static/css/main.9f29a650.chunk.css"
+  },
+  {
+    "revision": "c6edae855e838d003954",
+    "url": "/sfpopos/static/js/main.9e41f41f.chunk.js"
+  },
+  {
+    "revision": "811452051e0fe4425eac",
+    "url": "/sfpopos/static/js/runtime~main.97827f69.js"
+  },
+  {
+    "revision": "93ab334fc2ace4c8cfe9",
+    "url": "/sfpopos/static/js/2.c7a50c9b.chunk.js"
+  },
+  {
+    "revision": "4549b272a3239dc43a82",
+    "url": "/sfpopos/static/js/3.8f58f390.chunk.js"
+  },
+  {
+    "revision": "27297fc1f497122f537b2b077f4bc39d",
+    "url": "/sfpopos/index.html"
+  }
+];
